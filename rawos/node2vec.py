fbadc5d07a149d2e23af3a063d72a1af05eec9d5
Bastian Rieck <bastian@rieck.me>
2022-02-28 16:30:04 +0100

    Storing simple embedding

--- rawos/node2vec.py
+++ rawos/node2vec.py
@@ -2,14 +2,10 @@
 
 import argparse
 
-import os.path as osp
-
-import matplotlib.pyplot as plt
 import torch
 
-from sklearn.decomposition import PCA
+import numpy as np
 
-from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import Node2Vec
 from torch_geometric.utils import erdos_renyi_graph
 
@@ -19,9 +15,14 @@ def main(args):
     edge_index = erdos_renyi_graph(num_nodes, 0.25)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = Node2Vec(edge_index, embedding_dim=32, walk_length=10,
-                     context_size=5, walks_per_node=10,
-                     num_negative_samples=1, p=1, q=1, sparse=True).to(device)
+    model = Node2Vec(
+        edge_index,
+        embedding_dim=args.dimension,
+        walk_length=args.length,
+        context_size=args.context,
+        walks_per_node=args.num_walks,
+        num_negative_samples=1, p=1, q=1,
+        sparse=True).to(device)
 
     loader = model.loader(batch_size=64, shuffle=True, num_workers=4)
     optimizer = torch.optim.SparseAdam(list(model.parameters()), lr=0.01)
@@ -41,29 +42,30 @@ def main(args):
     def test():
         model.eval()
         z = model()
-        return torch.linalg.vector_norm(z)
+        return z, torch.linalg.vector_norm(z)
 
     for epoch in range(1, 50):
         loss = train()
-        norm = test()
+        z, norm = test()
+
         print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Norm: {norm:.4f}')
 
-    @torch.no_grad()
-    def plot_points():
-        model.eval()
-        z = model(torch.arange(num_nodes, device=device))
-        z = PCA(n_components=2).fit_transform(z.cpu().numpy())
-
-        plt.figure(figsize=(8, 8))
-        plt.scatter(z[:, 0], z[:, 1], s=20)
-        plt.axis('off')
-        plt.show()
-
-    plot_points()
-
+    np.savetxt(
+        'embedding.csv',
+        z.detach().cpu().numpy(),
+        delimiter='\t',
+        fmt='%.4f'
+    )
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--context', type=int, default=5)
+    parser.add_argument('-d', '--dimension', type=int, default=32)
+    parser.add_argument('-l', '--length', type=int, default=10)
+    parser.add_argument('-n', '--num-walks', type=int, default=10)
+
     args = parser.parse_args()
 
     main(args)
