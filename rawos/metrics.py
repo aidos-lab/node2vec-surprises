863b7a44dfeef747b1e02badc65c1704dfe6a119
Bastian Rieck <bastian@rieck.me>
2022-03-02 11:11:15 +0100

    Added simple metrics

---
+++ rawos/metrics.py
@@ -0,0 +1,28 @@
+"""Metrics and pseudo-metrics for comparing point clouds."""
+
+import numpy as np
+
+from sklearn.metrics import pairwise_distances
+
+
+def diameter(X, metric='euclidean'):
+    """Calculate diameter of a point cloud."""
+    distances = pairwise_distances(X, metric=metric)
+    return np.max(distances)
+
+
+def hausdorff_distance(X, Y, metric='euclidean'):
+    """Calculate Hausdorff distance between point clouds.
+
+    Calculates the Hausdorff distance between two finite metric spaces,
+    i.e. two finite point clouds.
+    """
+    X = np.asarray(X)
+    Y = np.asarray(Y)
+
+    distances = pairwise_distances(X=X, Y=Y)
+
+    d_XY = np.max(np.min(distances, axis=1))
+    d_YX = np.max(np.min(distances, axis=0))
+
+    return max(d_XY, d_YX)
