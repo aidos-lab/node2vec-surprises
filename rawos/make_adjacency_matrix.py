9ae53054e358de01c835d1b56e23c81da1a0a7bf
Bastian Rieck <bastian@rieck.me>
2022-04-14 09:51:05 +0200

    Simple script for creating adjacency matrices

---
+++ rawos/make_adjacency_matrix.py
@@ -0,0 +1,28 @@
+"""Create adjacency matrix of well-known graphs."""
+
+import argparse
+
+import networkx as nx
+import numpy as np
+
+
+def lm():
+    G = nx.generators.les_miserables_graph()
+    return G
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'NAME',
+        type=str,
+        help='Name of graph to generate'
+    )
+
+    args = parser.parse_args()
+
+    if args.NAME == 'lm':
+        G = lm()
+
+    A = nx.adjacency_matrix(G, weight=None).toarray()
+    np.savetxt('/tmp/A.txt', A, fmt='%.4f')
