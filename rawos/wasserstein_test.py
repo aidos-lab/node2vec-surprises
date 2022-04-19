7e8bf3d4da90bbe4fa38ad9a4ea275a276fb619a
Bastian Rieck <bastian@rieck.me>
2022-04-19 11:01:31 +0200

    Updated test script

--- rawos/wasserstein_test.py
+++ rawos/wasserstein_test.py
@@ -1,19 +1,26 @@
 """Testing `scipy` implementation of Wasserstein distance."""
 
+from scipy.stats import entropy
+from scipy.stats import energy_distance
 from scipy.stats import wasserstein_distance
+
+from scipy.spatial.distance import jensenshannon
 
 import numpy as np
 import ot
 
-u = np.asarray([1.0, 2.0, 3.0], dtype=np.float)
-v = u.copy()
-np.random.default_rng().shuffle(v)
+u = np.asarray([0.1, 0.2, 0.7], dtype=float)
+v = np.asarray([0.2, 0.1, 0.7], dtype=float)
+n = len(u)
+x = np.arange(n, dtype=float)
 
 print(u, v)
-print(wasserstein_distance(u, v))
-print(wasserstein_distance(np.arange(len(u)), np.arange(len(v)), u, v))
+print('Entropy:', entropy(u, v))
+print('Energy distance:', energy_distance(u, v))
+print('Jensen--Shannon distance:', jensenshannon(u, v))
+print('Wasserstein distance:', wasserstein_distance(u, v))
 
-M = np.ones((len(u), len(v)))
-M[np.diag_indices_from(M)] = 0.0
+M = ot.dist(x.reshape(n, 1), x.reshape(n, 1), metric='minkowski')
+M /= M.max()
 
-print(ot.emd2(u, v, M))
+print('EMD:', ot.emd2(u, v, M))
