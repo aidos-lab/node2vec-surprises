bd293ba214326c43b2088f994c61b702f92838f4
Bastian Rieck <bastian@rieck.me>
2022-04-28 09:33:36 +0200

    Updated sweep script to take into account `q` parameter

--- scripts/sweep.sh
+++ scripts/sweep.sh
@@ -8,13 +8,14 @@ NAME="N2VS"
 for CONTEXT in 5 10; do
   for DIMENSION in 16 32 64; do
     for LENGTH in 5 10 20; do
-      sbatch -p cpu_p               \
-             -J ${NAME}             \
-             -o "${NAME}_%j.out"    \
-             --cpus-per-task=4      \
-             --mem=4G               \
-             --nice=10000           \
-             --wrap "poetry run python ../rawos/node2vec.py --length $LENGTH --dimension $DIMENSION --context $CONTEXT --epochs 200 "
+      for Q in 1 2; do
+        sbatch -p cpu_p               \
+               -J ${NAME}             \
+               -o "${NAME}_%j.out"    \
+               --cpus-per-task=4      \
+               --mem=4G               \
+               --nice=10000           \
+               --wrap "poetry run python ../rawos/node2vec.py -q $Q --length $LENGTH --dimension $DIMENSION --context $CONTEXT --epochs 200 "
     done
   done
 done
